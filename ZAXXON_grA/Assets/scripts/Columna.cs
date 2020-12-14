d36dc9a19979a2d2c58fede5b02aad6f2a0713c5
Pablo18-web <72516173+Pablo18-web@users.noreply.github.com>
2020-12-14 20:08:45 +0100

    correcion de las columnas

--- ZAXXON_grA/Assets/scripts/Columna.cs
+++ ZAXXON_grA/Assets/scripts/Columna.cs
@@ -9,8 +9,8 @@ public class Columna : MonoBehaviour
     private Sphere sphere;
 
     private Vector3 MyPos;
-    //[SerializeField] Vector3 DestPos;
-    //private Vector3 FinalPos;
+    [SerializeField] Vector3 DestPos;
+    private Vector3 FinalPos;
 //publica porque lo usaremos en otros scripts
    public float mySpeed;
     
@@ -21,8 +21,10 @@ public class Columna : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //sphere = GetComponent<Sphere>();
-        //mySpeed = sphere.speed;
+        Nave = GameObject.Find("Sphere");
+
+        sphere = Nave.GetComponent<Sphere>();
+        mySpeed = sphere.speed;
       
       
        //la velocidad que le hemos asignado a las columnas
@@ -34,25 +36,27 @@ public class Columna : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // MyPos = transform.position;
-        // FinalPos = MyPos + DestPos * Time.deltaTime * mySpeed;
-        //transform.position = FinalPos;
-        //print(MyPos);
+       
+        MyPos = transform.position;
+         FinalPos = MyPos + DestPos * Time.deltaTime * mySpeed;
+            transform.position = FinalPos;
+        print(MyPos);
+       
        
     }
     //Creamos un void para el movimiento de las columnas y su destruccion
         void MovimientoColumna(){
         
-       //Ese transform coge la posicion del objeto lo mueve hacia atras para cambiar la direccion cambia el .back / .up .down .forward .left .right 
-       //para cambiar la velocidad a la que se mueven estas columnas cambio el mySpeed
-        transform.Translate(Vector3.back * Time.deltaTime * mySpeed);
+           //Ese transform coge la posicion del objeto lo mueve hacia atras para cambiar la direccion cambia el .back / .up .down .forward .left .right 
+           //para cambiar la velocidad a la que se mueven estas columnas cambio el mySpeed
+            transform.Translate(Vector3.back * Time.deltaTime * mySpeed);
        
-       //este if coge la posicion de tu objeto asignado y si sobrepasa la posicion lo destruye
-        if(transform.position.y > 25)
-        {
-            //destroy es un metodo de unity y si pones gameobject porque lo  que buscas es destruir el objeto entero
-            Destroy(gameObject);
-        }
+           //este if coge la posicion de tu objeto asignado y si sobrepasa la posicion lo destruye
+            if(transform.position.y > 25)
+            {
+                //destroy es un metodo de unity y si pones gameobject porque lo  que buscas es destruir el objeto entero
+                Destroy(gameObject);
+            }
 
 
 
