ad1aa266a8c2b2ce787ca34d0547c6aa31716874
Iblaz <Igvemos@hotmail.es>
2020-12-14 18:48:54 +0100

    commit de la clase de los arrays (ajedrez)

---
+++ ZAXXON_grA/Assets/scripts/Initgame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Initgame : MonoBehaviour
+{
+    [SerializeField] GameObject[] objetos;
+    Vector3 initPos = new Vector3(-21, 0, 21); 
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int r = Random.Range(0, objetos.Length);
+        Instantiate(objetos[r], initPos, Quaternion.Euler(-90,90,0));
+    }
+}
