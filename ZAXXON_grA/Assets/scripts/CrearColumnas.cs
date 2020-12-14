84b14ff010d92ee0539cf9a10e5134bebd3f88d0
Pablo18-web <72516173+Pablo18-web@users.noreply.github.com>
2020-12-14 17:43:01 +0100

    Trabajo de clase
    Trabajo de clase

--- ZAXXON_grA/Assets/scripts/CrearColumnas.cs
+++ ZAXXON_grA/Assets/scripts/CrearColumnas.cs
@@ -8,13 +8,16 @@ public class CrearColumnas : MonoBehaviour
     [SerializeField] GameObject MyColumn;
     //Variable de tipo Transform que contendrá el objeto de referencia
     [SerializeField] Transform RefPos;
+   //Es una variable con la que le vamos a decir a nuestro metodo IniciarColumnas que tenga la  distancia entre ellas
+    [SerializeField] float distobstacle;
+    
         
     // Start is called before the first frame update
     void Start()
     {
+       //Startcoroutine es lo que tenemos que poner cuando queremos que se inicie una corrutina abres parentesis y entre comillas pones el nombre que le has puesto
         StartCoroutine("ColumnCorrutine");
-
-
+        InicioColumnas();
     }
 
     // Update is called once per frame
@@ -29,25 +32,53 @@ public class CrearColumnas : MonoBehaviour
 
     void CrearColumna()
     {
-        //Creo un nuevo vector3
+        //Crear una variable para que genere un numero aleatorio entre los dos rangos que le asigno
         float posRandom = Random.Range(0f, 30f);
-        Vector3 DestPos = new Vector3(0, 0, posRandom);
+        //Creas un nuevo vector que va a ser el destino donde se genere la columna, el posramdom al principio debido a que queremos que se genere en el eje x en el centro seria el eje y , z en ultimo lugar
+        Vector3 DestPos = new Vector3(posRandom, 0, 0);
+       //Este vector coge la posicion de referencia y le añade ese vector aleatorio
         Vector3 NewPos = RefPos.position + DestPos;
         //Instancio el prefab en la posición del objeto de referencia
         //Como tenemos su componente Transform, le indicamos que lo que quiero es su posición
         Instantiate(MyColumn, NewPos, Quaternion.identity);
     }
-
+    //corrutina es un metodo/funcion que se puede realizar sin parar el codigo
     IEnumerator ColumnCorrutine()
     {
-
+        //for(puntodeinicio,condicion,accionfinal) este bucle nos sirve para crear la columna cada x tiempo
         for (int n=0; ; n++ )
         {
+            //para ver las columnas que estoy generando
             print(n);
             //Intancio el prefab en coordenadas 0,0,0
-            //Instantiate(MyColumn);
+            //Instantiate(MyColumn); esto solo es necesario si me pide que lo cree en 0,0,0
+            //aqui como queremos que se genere en la posicion ramdom asignamos el metodo que hemos creado para que haga esto
             CrearColumna();
+          //Dentro del for hacemos un yield return que basicamente es decirle al for que pare antes de volver hacerse
             yield return new WaitForSeconds(1);
         }
     }
+
+    void InicioColumnas(){
+
+
+
+
+           distobstacle = 10f;
+        //creamos un bucle que dependa del numero de columnas que quiera crear
+        for (int n = 1; n < 20; n++) ;
+
+            /*
+        {
+           //hacemos lo mismo que en crear columna pero ahora en eje z le metemos la variable que hemos creado de distobstacle
+            float randomX = Random.Range(0f, -30f);
+            NewPos = new Vector3(randomX, 0, n * distobstacle);
+            Vector3 finalPos = RefPos.position - NewPos;
+            Instantiate(MyColumn, finalPos, Quaternion.identity);
+        }
+            */
+    }
+
+
+
 }
--- ZAXXON_grA/Assets/scripts/Sphere.cs
+++ ZAXXON_grA/Assets/scripts/Sphere.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class Sphere : MonoBehaviour
 {
-    public float speed = 2.5f;
+    //Declaramos la velocidad de la nave fuera
+    public float speed;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        //aqui asignamos la velocidad para que siempre empieze con esta
+        speed = 2.5f;
     }
 
     // Update is called once per frame
@@ -23,12 +25,87 @@ public class Sphere : MonoBehaviour
 
     void MoverNave()
     {
+        //para comprobar el desplazamiento en x
         print(transform.position.x);
+       //aqui asignamos el desplazamiento a los controles del mando/teclado vertical
         float desplY = Input.GetAxis("Vertical");
+       //Esto es para mover la nave de arriba 
         transform.Translate(Vector3.up * Time.deltaTime * speed * desplY);
+        //aqui asignamos el desplazamiento en horizontal
         float desplX = Input.GetAxis("Horizontal");
+        //mover en el eje horizontal     
         transform.Translate(Vector3.right * Time.deltaTime * speed * desplX);
+
+        //creamos una variable que compruebe la posicion del objeto que esta asignado al script
+        float PosX = transform.position.x;
+        float PosY = transform.position.y;
+        //Se mueve siempre que este en las posiciones marcadas en este caso -11 y -3
+        if (PosX > -11 && PosX < -3)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed * desplX);
+        }
+        //Esto dice: Si te pasas de -11 pero esta intentando volver dejalo moverse
+        else if (PosX < -11 && desplX > 0)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed * desplX);
+        }
+       //Esto dice: Si te pasas de -3 pero esta intentando volver dejalo moverse
+        else if (PosX > -3 && desplX < 0)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed * desplX);
+        }
+
+       /*Es el metodo resumido del movimiento restringido
+        float posX = transform.position.x;
+        float posY = transform.position.y;
+        float desplY = Input.GetAxis("Vertical");
+        float desplX = Input.GetAxis("Horizontal");
+
+        //restringir movimiento en el eje X
+        if (posX < 14 && posX > -14 || posX < -14 && desplX > 0 ||  posX > 14 && desplX < 0)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed * desplX);
+        }
+
+        //restringir movimiento en el eje Y
+        if (posY < 10 && posY > 1 || posY < 1 && desplY > 0 || posY > 10 && desplY < 0)
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * speed * desplY);
+        }
+        */
+
+        /*
+        int suma1 = 19;
+        int suma2 = 11;
+        int result;
+        
+        //siemrpe que llame a sumitas me va a dar el resultado de 19+11
+        int Sumitas(){
+            result= suma1 + suma2;
+            returm = result;
+        //para hacer una funcion que cada vez que la llame sume los numero que yo le pida
+        
+        int Sumas(int a , int b){
+            result= a+b;
+            return = result;
+            //en el metodo Start ponemos la funcion Sumas(y aqui ponemos los enteros que quiere que sume)
+            Sumas(2,4)
+            Sumas(3,6)
+
+        }
+    */
+        }
+
+    // PARA USERI INTERFACE
+    //añadir arriba del todo using UnityEngine.UI;
+    //un serializedfield de tipo de texto
+    //Lo asignas en unity
+    //Creas un método en el que se va a cambiar el texto si a lo largo del juego es un texto que va cambiando como un contador se metera en el update en el caso contrario en el star
+    // DISTANCIA 
+    // DISTANCIA.text = "DISTANCIA"
+
+
 
 
     }
-}
+
