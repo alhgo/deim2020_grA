239b408eea9cc92a177303b066abea7d1be77201
Alfrisco <73118742+Alfrisco@users.noreply.github.com>
2020-12-14 20:06:05 +0100

    Chess Game zaxxon
    No tengo mucho hecho porque he tenido un problema desde el principio, creo que con la importación de las fichas a unity, y luego al hacer el script ha ido en picado, en definitiva, no he avanzado nada, sé como funciona el array y como se usa en este caso por ejemplo, es decir con que fin pero no lo he podido ejecutar.

---
+++ ZAXXON_grA/Assets/ChessGame/Assets/Pawn.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pawn : MonoBehaviour
+{
+
+[SerilizeField] GameObject[] objetos;
+
+Vector3 initPosWhite = new Vector3(-21, 0, 21);
+Vector3 initPosBlack = new Vector3(-21, 0, 21);
+Vector3 initPosPawn = new Vector3(-21, 0, 15);
+    // Start is called before the first frame update
+    void Start()
+    {
+            for(int n= 0; n < 8; n++){
+                     Instantiate(objetos[0], initPosWhite, Quaternion.Euler(-90,0,90));
+                initPosWhite = initPosWhite + new Vector3(6, 0, 0);
+            }
+       
+             for(int p= 0; p < 8; p++){
+                     Instantiate(objetos[0], initPosPawn, Quaternion.Euler(-90,0,90));
+                initPosPawn = initPosPawn + new Vector3(6, 0, 0);
+
+
+            for(int n= 0; n < 8; n++){
+                     Instantiate(objetos[0], initPosBlack, Quaternion.Euler(-90,0,90));
+                initPosBlack = initPosBlack + new Vector3(6, 0, 0);
+            }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ZAXXON_grA/Assets/ChessGame/Assets/scripts/Pawn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pawn : MonoBehaviour
+{
+
+[SerilizeField] GameObject[] objetos;
+
+Vector3 initPosWhite = new 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(objetos[0], initPosWhite, Quaternion.Euler(-90,0,90));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
