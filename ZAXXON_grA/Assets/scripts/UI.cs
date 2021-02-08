266c6e9adcf343d2912215f9e68be136ffb76fde
TheTibus <72516070+TheTibus@users.noreply.github.com>
2021-02-08 16:30:14 +0100

    Introducción sistema de partículas.

--- ZAXXON_grA/Assets/scripts/UI.cs
+++ ZAXXON_grA/Assets/scripts/UI.cs
@@ -2,23 +2,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class UI : MonoBehaviour
 {
+    [SerializeField] Text TextoTiempo;
+    [SerializeField] Text TextoPuntuacion;
     public float playedTime;
+    float tiempo;
+    float segundos;
+    float minutos;
+    void Start()
+    {
 
-void Start(){
-     
-     playedTime = 0;
-      
- }    
- 
- void Update(){
-     
-     playedTime = Time.time * 10;
-    
-    print(playedTime + " es tu tiempo");
+        playedTime = 0;
 
-    
- } 
+    }
+
+    void Update()
+    {
+
+        playedTime += Time.deltaTime;
+        ConversorTiempo();
+        TextoTiempo.text = "Tiempo en partida: " + minutos.ToString("00") + ":" + segundos.ToString("00");
+        float puntuacion = playedTime * 20;
+        TextoPuntuacion.text = "Puntuación: " + puntuacion.ToString("00");
+
+        
+
+
+    }
+
+    void ConversorTiempo()
+    {
+        {
+            segundos = (int)playedTime % 60;
+            minutos = (int)((playedTime / 60) % 60);
+        }
+    }
 }
