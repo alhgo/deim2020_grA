aa40cd6b4f2b1bb48757d24077183cf95c1b7eaa
iratxedominguez2 <71842189+iratxedominguez2@users.noreply.github.com>
2021-02-13 16:05:03 +0100

    Música en el menú de inicio, High Scores y sonido en los botones.
    He añadido Audio Source en el Menú de inicio y en la pantalla del High Score. También he puesto un efecto para que cuando pulses los botones se escuche ejecutándolo con un script y Audio Source.

---
+++ ZAXXON_grA/Assets/scripts/SonidoBotones.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SonidoBotones : MonoBehaviour
+{
+    //Para saber de dónde viene el sonido
+    public AudioSource fuente;
+    //Qué sonido tiene que emitir
+    public AudioClip sonido;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fuente.clip = sonido;
+    }
+
+    // Update is called once per frame
+    public void Reproducir()
+    {
+        fuente.Play();
+    }
+}
