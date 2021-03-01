acc9aa347cb77e9eb0198aa18602b24b5a4bc7f9
LEspadaC <72516160+LEspadaC@users.noreply.github.com>
2021-03-01 18:15:09 +0100

    5.1- Velocidad de desplazamiento
    El contador de distancia está ahora dentro de una corrutina.

--- ZAXXON_grA/Assets/scripts/Distance.cs
+++ ZAXXON_grA/Assets/scripts/Distance.cs
@@ -9,10 +9,34 @@ public class Distance : MonoBehaviour
     public Text distanciaText;
     public float distancia = 0.0f;
 
-    public void Update()
+    public void Start()
+    {
+        StartCoroutine("DistanceCorrutine");
+    }
+    void CheckDistance() 
+    {
+        //distancia += Time.deltaTime;
+        distancia += 1;
+        //print(distancia);
+        //print(Time.deltaTime);
+        distanciaText.text = "" + distancia.ToString("f0") + "m";
+    }
+
+    IEnumerator DistanceCorrutine()
+    {
+        int n;
+        for (n = 0; ; n++)
+        {
+            CheckDistance();
+            yield return new WaitForSeconds(1);
+        }
+
+    }
+
+    /*public void Update()
     {
         distancia += Time.deltaTime;
         distanciaText.text = "" + distancia.ToString("f0") + "m";
-    }
+    }*/
 
 }
