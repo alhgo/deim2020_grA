26a69852df7693d801aa282286378ef5e91caffe
LEspadaC <72516160+LEspadaC@users.noreply.github.com>
2021-03-01 17:53:25 +0100

    5- Velocidad de desplazamiento
    Se ha creado el contador de distancia.

---
+++ ZAXXON_grA/Assets/scripts/Distance.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Distance : MonoBehaviour
+{
+    public Text distanciaText;
+    public float distancia = 0.0f;
+
+    public void Update()
+    {
+        distancia += Time.deltaTime;
+        distanciaText.text = "" + distancia.ToString("f0") + "m";
+    }
+
+}
